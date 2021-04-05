b4e8e0d50c7217567d5cc4e33d3329fa2c57ce23
Marlon <marlonsk48@gmail.com>
2021-04-04 21:02:15 -0500

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Wordl</h1>
+        <h1>Marlon Muñoz</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
